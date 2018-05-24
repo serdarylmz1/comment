341e8ba56105b9560f6e469727137133ed6474b9
serdarylmz1 <36953935+serdarylmz1@users.noreply.github.com>
2018-05-24 08:55:54 +0300

    Update update.php

--- views/backend/comments/update.php
+++ views/backend/comments/update.php
@@ -5,14 +5,14 @@ use yii\helpers\Html;
 /* @var $this yii\web\View */
 /* @var $model kouosl\comment\models\Comment */
 
-$this->title = 'Update Comment: ' . $model->title;
+$this->title = 'Update Comment: ' . $model->username;
 $this->params['breadcrumbs'][] = ['label' => 'Comments', 'url' => ['index']];
-$this->params['breadcrumbs'][] = ['label' => $model->title, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = ['label' => $model->username, 'url' => ['view', 'id' => $model->id]];
 $this->params['breadcrumbs'][] = 'Update';
 ?>
 <div class="comment-update">
 
-    <h1><?= Html::encode($this->title) ?></h1>
+    <h1><?= Html::encode($this->username) ?></h1>
 
     <?= $this->render('_form', [
         'model' => $model,
